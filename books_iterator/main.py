0ff496479e82f7831d9f24f823d8c20e488cb047
James <jim-hart@users.noreply.github.com>
2017-06-10 04:11:13 -0400

    Initial commit -- all unittests passed locally

--- books_iterator/main.py
+++ books_iterator/main.py
@@ -1,26 +1,29 @@
 from decimal import Decimal
 
-
 class Book(object):
     def __init__(self, title, authors, price_amount, price_currency):
         # initialize attributes
-        pass
+        self.title = title
+        self.authors = authors
+        self.price_currency = price_currency
+        self.price_amount = price_amount
 
     @property
     def price(self):
         # create an instance of `Price`, using the book's attributes
-        pass
+        return Price(self.price_amount, self.price_currency)
 
     def __str__(self):
         # check the string format in the unit tests
-        pass
+        return "{} (by {}) - {}${}".format(
+               self.title, self.authors, self.price_currency, self.price_amount)
 
 
 class Price(object):
     EXCHANGE_RATES = {
         'USD': {
             'EUR': Decimal('0.89'),
-            'YEN': Decimal('109.8')
+            'YEN': Decimal('109.8'),
         },
         'EUR': {
             'USD': Decimal('1.13'),
@@ -33,60 +36,63 @@ class Price(object):
     }
 
     def __init__(self, amount, currency='USD'):
-        pass
-
-    def __str__(self):
-        pass
+        self.amount = amount
+        self.currency = currency
 
     def get_currency(self):
-        pass
-
-    def __add__(self, other):
-        # return a new `Price` instance, representing the sum of
-        # both given ones
-        pass
-
-    def __eq__(self, other):
-        # compare if two prices are equal. Keep in mind that both prices
-        # might have different currencies. Use the `.get_value()` function
-        # to transform prices to a comparable currency.
-        pass
-
-    def __ne__(self, other):
-        # opposite to __eq__
-        pass
+        return self.currency
 
     def get_value(self, currency=None):
         # if no currency is given, returns the current price amount. If a
         # different currency is given, handles the price convertion to the
         # given currency. Use the `EXCHANGE_RATES` dict for that.
-        pass
+        if not currency or (currency == self.currency):
+            return self.amount
+        else:
+            return Price.EXCHANGE_RATES[self.currency][currency] * self.amount
+
+    def __str__(self):
+        return "{} {}".format(self.currency, self.amount)
+
+    def __add__(self, other):
+        # return a new `Price` instance, representing the sum of
+        # both given ones
+        return Price(self.get_value() + other.get_value(self.currency))
+
+    def __eq__(self, other):
+        # compare if two prices are equal. Keep in mind that both prices
+        # might have different currencies. Use the `.get_value()` function
+        # to transform prices to a comparable currency.
+        if self.get_value('USD') == other.get_value('USD'):
+            return True
+
+    def __ne__(self, other):
+        # opposite to __eq__
+        if self.get_value('USD') != other.get_value('USD'):
+            return False
 
 
 class BookIterator(object):
     def __init__(self, file_path):
-        pass
+        self.values = read_file_line_by_line(file_path)
+        self.count = 0
 
     def __iter__(self):
-        pass
+        return self
 
     def __next__(self):
-        # make sure each execution of __next__ returns an instance
-        # of the `Book` class.
-        pass
+        if self.count < len(self.values):
+            self.count += 1
+            return Book(*self.values[self.count-1])
+        else:
+            self.count = 0
+            raise StopIteration
 
-    next = __next__
+        next = __next__
 
 
 def read_file_line_by_line(file_path):
-    file_lines = []
-    file_obj = open(file_path, 'r')
-    try:
-        line = file_obj.readline()
-        while line:
-            book_data = [l.strip() for l in line.split(',')]
-            file_lines.append(book_data)
-            line = file_obj.readline()
-    finally:
-        file_obj.close()
-    return file_lines
+    with open(file_path, 'r') as f_obj:
+        return [line.rstrip().split(',') for line in f_obj.readlines()]
+
+
